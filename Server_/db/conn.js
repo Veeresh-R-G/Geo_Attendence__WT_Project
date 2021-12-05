17e0c3514eaf5f4996fb0ffdddfb4863a7bedab1
Veeresh R G <72454785+Veeresh-R-G@users.noreply.github.com>
2021-12-05 20:27:29 +0530

    Delete Server Files

--- Server_/db/conn.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-//DataBase Connection Link
-const DB_Connection = 'mongodb://localhost:27017/projectDB'
-mongoose.connect(DB_Connection, { useNewUrlParser: true })
